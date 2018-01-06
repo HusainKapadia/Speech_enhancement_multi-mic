5a10d7a624956dc89e0cd24dcbd61d88354ebca0
Husain <husainkapadia2811@gmail.com>
2018-01-06 16:40:43 +0100

    final

--- demo.m
+++ demo.m
@@ -58,7 +58,7 @@ end
 %% Plots
 figure()
 subplot(3,1,1)
-plot3(y,1:N,1:m), title('Noisy Speech');
+plot(y), title('Noisy Speech');
 subplot(3,1,2)
 plot(s_e), title('Corrected Speech');
 subplot(3,1,3)
@@ -66,6 +66,8 @@ plot(s), title('Clean Speech');
 
 figure()
 stem(var_emp, 'r', 'DisplayName', 'Empirical Variance')
+xlabel('No. of Microphones')
+ylabel('CRLB / Empirical Variance')
 hold on;
 stem(CRLB, 'b', 'DisplayName', 'CRLB')
 title('CRLB and Empirical Variance');
@@ -73,20 +75,6 @@ legend('show');
  
 figure()
 stem(mse), title('Mean Square Error');
-
-P2 = permute(Y, [3 1 2]);
-for i = 1:size(Y,1)
-    Cw2 = zeros(i);
-    for k = 1:200
-        U2 = P2(:,1:i,k);
-        Cw2 = (k*Cw2 + cov(U2))/(k+1);
-    end
-    h = ones(1,i);
-    CRLBpf(i) = real(inv(h*inv(Cw2)*h'));
-end
-
-figure()
-stem(CRLBpf), title('CRLB per frequency band');
 
 %% Sound
 sound(0.1*s_e, fs)
